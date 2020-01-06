a8879593a37f42ad63e56f02db7060edc6596929
wmui <qq22337383@gmail.com>
2020-01-06 18:13:35 +0800

    init

--- base/valueOf.js
+++ base/valueOf.js
@@ -12,3 +12,14 @@ var obj = {age: 1}
 var obj2 = obj.valueOf()
 obj2.age = 2
 console.log(obj); // { age: 2}
+
+
+// 对象参与运算时，先调用 valueOf()方法，如果结果为NaN，再调用 toString() 方法
+var a = {
+    valueOf: function () {
+        return 1
+    }
+}
+
+var b = a + 1
+console.log(b); // 2
